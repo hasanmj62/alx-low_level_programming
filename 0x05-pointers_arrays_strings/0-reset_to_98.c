8312b8a56aa38ac828d32ab3e43c09255eb5ae80
hasanmj62 <hasanmj62@gmail.com>
2023-03-28 01:29:35 +0200

    reset-to-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void swap(int *x)
+{
+
+    *x = 98;
+}
+int main(void)
+{
+
+    int n = 420;
+    printf("n is : %d\n", n);
+    swap(&n);
+    printf("n is : %d\n", n);
+}
