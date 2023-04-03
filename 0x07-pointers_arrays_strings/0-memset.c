431c192683a2bf755d78c2484105010fa14d9f65
hasanmj62 <hasanmj62@gmail.com>
2023-04-03 23:58:55 +0200

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
- * Return: s
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		unsigned int i;
-
-		for (i = 0; i < n; i++)
-		{
-			s[i] = b;
-		}
-
-		return (s);
+		s[i] = b;
 	}
+
+	return (s);
+}
