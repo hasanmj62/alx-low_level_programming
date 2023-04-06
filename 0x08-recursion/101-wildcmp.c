c8a7bf51ab39ae15b138131881b6b745cb35f341
hasanmj62 <hasanmj62@gmail.com>
2023-04-06 02:15:45 +0200

    compare to string

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
  * wildcmp - compares two string and returns 1
  * if the string can be consider ident
  * else return 0.
- * @s1: the default string 
+ * @s1: the default string
  * @s2: the special string that contain ("*")
  *
  * Return: 1 if identical, else 0
