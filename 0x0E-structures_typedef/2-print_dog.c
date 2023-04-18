6d4095e64720ecaccb8526edb6ead02f773912ef
hasanmj62 <hasanmj62@gmail.com>
2023-04-18 04:31:38 +0200

    updates

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,5 @@ d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
 
-pritf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
