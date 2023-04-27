75f387b2297c62677bb6094bfe8fe4ffd91d84ee
hasanmj62 <hassanmj62@gmail.com>
2023-04-27 16:32:43 +0200

    LinkedList-Adv

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,7 @@ void first(void) __attribute__ ((constructor));
 
 void first(void)
 {
-printf("You're bead! and yet, you must allow, \n");
+printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
+
 }
