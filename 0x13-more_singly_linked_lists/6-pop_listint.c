db0602c1214b0f99b4869b3f9dbfb428bb3e247c
hasanmj62 <hassanmj62@gmail.com>
2023-05-02 20:29:37 +0300

    tas6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,17 +6,18 @@
 * Return: the data inside the elements that was deleted .
 * || 0  if the list is empty.
 */
-
-int pop_listint(pop_listint_t **head)
+int pop_listint(listint_t **head)
 {
 listint_t *temp;
 int num;
-if (!head || *head)
+
+if (!head || !*head)
 return (0);
+
 num = (*head)->n;
 temp = (*head)->next;
 free(*head);
 *head = temp;
 
-return (num);
+	return (num);
 }
