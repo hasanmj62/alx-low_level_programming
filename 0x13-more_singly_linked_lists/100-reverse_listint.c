86214f516a40301d37d263150ba44eba7b6e5ba4
hasanmj62 <hassanmj62@gmail.com>
2023-05-02 20:59:47 +0300

    taskmandatory

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,15 +9,18 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
-listint_t *back = NULL;
-listint_t *forward = NULL;
-while (*head)
-{
-forward = (*head)->forward;
-(*head)->forward = back;
-back = *head;
-*head = forward;
-}
-*head = back;
-return (*head);
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+
+	return (*head);
 }
